5f122b8d398d0381223a129f5cc68dab5a513a5e
Alexandre Wotawa <awotawa@gmail.com>
2022-05-03 08:18:30 +0200

    Serialization and Serialization Groups for Media

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -11,6 +11,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ApiResource(
   collectionOperations: ['get', 'post'],
   itemOperations: ['get', 'patch', 'delete'],
+  normalizationContext: ['groups' => ['media:read']],
+  denormalizationContext: ['groups' => ['media:write']],
 )]
 class Media
 {
@@ -20,14 +22,17 @@ class Media
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(["media:read", "media:write"])]
     private $type;
 
     #[ORM\Column(type: 'string', length: 255)]
     #[Assert\Regex(['pattern' => "/(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&=]*))/"])]
+    #[Groups(["media:read", "media:write"])]
     private $link;
 
     #[ORM\ManyToOne(targetEntity: Lodging::class, inversedBy: 'media')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(["media:read"])]
     private $lodging;
 
     public function getId(): ?int
