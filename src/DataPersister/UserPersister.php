dccd51adacf47cfb97d133db446d2cbf54d06872
Alexandre Wotawa <awotawa@gmail.com>
2022-05-05 01:15:51 +0200

    recopying the files and structure for the JWT that Jhessy had and that was working

---
+++ src/DataPersister/UserPersister.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataPersister;
+
+use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserPersister implements DataPersisterInterface
+{
+  protected $em;
+
+  public function __construct(EntityManagerInterface $em)
+  {
+    $this->em = $em;
+  }
+
+  public function supports($data): bool
+  {
+    return $data instanceof User;
+  }
+
+  public function persist($data): void
+  {
+    // 1. Mettre une date de création sur le lodging
+    // $data->setCreatedAt(new \DateTime());
+    // 2. Mettre une date de update sur le lodging
+    // dd($data->getPassword());
+    $hashedPassword = hash('sha256', $data->getPassword());
+    // dd($hashedPassword);
+    $data->setPassword($hashedPassword);
+    // 3. Ask Doctrine to persist the data
+    $this->em->persist($data);
+    // 4. Send data to the DB
+    $this->em->flush();
+  }
+
+  public function remove($data): void
+  {
+    // 1. Demander à doctrine de supprimer le lodging
+    $this->em->remove($data);
+    $this->em->flush();
+  }
+}
