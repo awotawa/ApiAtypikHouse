eca6dd3a4fe8d6fe620885ec537eef21d5a75de4
Alexandre Wotawa <awotawa@gmail.com>
2022-05-12 17:46:05 +0200

    test entity for the json formatting

---
+++ src/Entity/Test.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\TestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TestRepository::class)]
+#[ApiResource]
+class Test
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'json')]
+    private $name = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?array
+    {
+        return $this->name;
+    }
+
+    public function setName(array $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
