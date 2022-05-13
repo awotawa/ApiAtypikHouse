cefbf830f5d9ce1c23d71d3cf712537ff5aa2114
aruseus98 <aruseus98@gmail.com>
2022-05-13 19:52:32 +0200

    Correction users

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -70,7 +70,7 @@ class User implements UserInterface, JWTUserInterface, PasswordAuthenticatedUser
     private $email;
 
     #[ORM\Column(type: 'json')]
-    #[Groups(["user:read", "user:write"])]
+    #[Groups(["user:read"])]
     private $roles = [];
 
     #[Assert\NotBlank()]
